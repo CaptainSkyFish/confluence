99e12f50bfb153cfb9beca35a12cde5ce87f5998
CaptainSkyFish <ajaystd9756@gmail.com>
2025-05-11 18:47:26 +0530

    test

--- backend/app.js
+++
@@ -1,9 +0,0 @@
-var http = require('http');
-
-var server = http.createServer(function (req, res) {
-    res.writeHead(200);
-    res.end('Hello Azure');
-});
-
-setInterval(() => { console.log("server up") }, 5000)
-server.listen(process.env.PORT);
