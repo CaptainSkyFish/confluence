77e032ff7fad17d5493cff1c41c284d70e9c8f75
CaptainSkyFish <ajaystd9756@gmail.com>
2025-06-30 08:45:40 +0530

    trigger azure static app deployment

--- frontend/src/api/getCurrentUser.ts
+++ frontend/src/api/getCurrentUser.ts
@@ -1,8 +1,8 @@
 import { axiosInstance } from "../config/axiosInstance";
 
 const getCurrentUser = async () => {
-  const response = await axiosInstance.get("/api/v1/users/me");
   console.log("Frontend sees:", import.meta.env.VITE_BACKEND_URL);
+  const response = await axiosInstance.get("/api/v1/users/me");
   if (response.status === 401) return null;
   return response.data;
 };
