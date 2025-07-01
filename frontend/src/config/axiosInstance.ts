a5479e03ae154fe27548686bacd506b0335de11e
CaptainSkyFish <ajaystd9756@gmail.com>
2025-07-01 23:25:39 +0530

    SWA fix deployment

--- frontend/src/config/axiosInstance.ts
+++ frontend/src/config/axiosInstance.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import useToastStore from "../store/toastStore";
 
 export const axiosInstance = axios.create({
-  baseURL: `${process.env.VITE_BACKEND_URL}`,
+  baseURL: `${import.meta.env.VITE_BACKEND_URL}`,
   withCredentials: true,
 });
 
